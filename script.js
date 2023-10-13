6a8cbc7481c77021a921e2322c929fb7373b9719
Aureltus <aureltusr@gmail.com>
2023-10-13 16:02:03 +0200

    script

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-<<<<<<< HEAD
 let button = document.querySelector(".btn");
 function random(nb) {
   return Math.floor(Math.random() * nb);
 }
+
 function Reset() {
   button.innerText = "Random apprenant";
 }
+
 function choixApprenant() {
   let TableauApprenant = [
     "Elodie",
@@ -22,14 +23,8 @@ function choixApprenant() {
   let index = random(10);
   button.innerText = TableauApprenant[index];
 }
-addEventListener("button.onclick", (event) => {
-  choixApprenant();
-  setTimeout(() => Reset(), 3000);
-});
+
 button.onclick = () => {
   choixApprenant();
   setTimeout(() => Reset(), 3000);
 };
-=======
-test;
->>>>>>> 62d589c194d68bfe2f4bc8fd7f31e8789ea94d32
